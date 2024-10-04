39f9c0a80365e2c00c3ac49e1f401011ed95225e
Thiyunu <dinal.bandara@gmail.com>
2024-10-04 14:17:37 +0530

    feat: removed web socket service from the private rooms

--- components-multiplayer/start-game-pool/start-game-pool-private.tsx
+++ components-multiplayer/start-game-pool/start-game-pool-private.tsx
@@ -6,11 +6,18 @@ import {
   SocketData,
   StartGamePoolPrivateProps,
 } from "@/utils/types-multiplayer";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import MyName from "./my-name";
+import OpponentsName from "./opponents-name";
 
 const webSocketURL = process.env.NEXT_PUBLIC_WEBSOCKET_URL;
+type Props = {
+  roomId: string;
+};
 
 const StartGamePoolPrivate = (props: StartGamePoolPrivateProps) => {
   const [roomData, setRoomData] = useState<SocketData[]>([]);
@@ -24,59 +31,82 @@ const StartGamePoolPrivate = (props: StartGamePoolPrivateProps) => {
   const [isRoomCreator, setIsRoomCreator] = useState<boolean>(true);
   const [roomCreator, setRoomCreator] = useState<SocketData | null>(null);
 
-  const getRoomData = () => {
-    SocketManager.getRoomData(
-      roomId,
+  const roomdataFromDB = useQuery(api.rooms.getRoomData, { roomName: roomId });
+  const joinRoomDB = useMutation(api.rooms.joinRoom);
+  const isOpponentJoinedDB = useQuery(api.rooms.isOpponentJoined, {
+    roomName: roomId,
+  });
+  const isRoomCreatorDB = useQuery(api.rooms.isRoomCreator, {
+    roomName: roomId,
+    userName: userName || "",
+  });
+  const updateRoomStatustoJoined = useMutation(
+    api.rooms.updateRoomStatustoJoined
+  );
+  const PlayersJoined = useQuery(api.rooms.isPlayersJoined, {
+    roomName: roomId || "",
+  });
 
-      (data: { creator: SocketData; players: SocketData[] }) => {
-        setRoomData(data.players);
-        setRoomCreator(data.creator);
-        console.log("data.creator:", data.creator);
-        console.log(
-          "data.creator.username !== UnKnown",
-          data.creator.username !== "UnKnown"
-        );
+  const hasJoinedRoom = useRef(false);
+  useEffect(() => {
+    if (PlayersJoined) {
+      router.push(`/multiplayer/gameplay/public/${roomId}`);
+    }
+  }, [PlayersJoined]);
 
-        // Check if the creator's username is not "Unknown"
-        if (data.creator.username && data.creator.username !== "Unknown") {
-          // Save the room creator in local storage
-          localStorage.setItem(
-            `roomCreator_${roomId}`,
-            JSON.stringify(data.creator.username)
-          );
-          console.log(
-            "Room creator saved in localStorage:",
-            data.creator.username
-          );
-        } else {
-          console.log("Room creator is 'Unknown', not saving to localStorage.");
-        }
+  // const getRoomData = () => {
+  //   SocketManager.getRoomData(
+  //     roomId,
 
-        console.log("Room Data:", data);
+  //     (data: { creator: SocketData; players: SocketData[] }) => {
+  //       setRoomData(data.players);
+  //       setRoomCreator(data.creator);
+  //       console.log("data.creator:", data.creator);
+  //       console.log(
+  //         "data.creator.username !== UnKnown",
+  //         data.creator.username !== "UnKnown"
+  //       );
 
-        // Set opponent player if there are enough players
-        if (data.players.length > 1) {
-          setOpponentPlayer(data.players[0]);
-        }
+  //       // Check if the creator's username is not "Unknown"
+  //       if (data.creator.username && data.creator.username !== "Unknown") {
+  //         // Save the room creator in local storage
+  //         localStorage.setItem(
+  //           `roomCreator_${roomId}`,
+  //           JSON.stringify(data.creator.username)
+  //         );
+  //         console.log(
+  //           "Room creator saved in localStorage:",
+  //           data.creator.username
+  //         );
+  //       } else {
+  //         console.log("Room creator is 'Unknown', not saving to localStorage.");
+  //       }
 
-        // Update isRoomCreator based on the creator username
-        const currentUserIsCreator = data.creator?.username === userName;
-        setIsRoomCreator(currentUserIsCreator);
+  //       console.log("Room Data:", data);
 
-        if (data.creator.username && data.creator.username === "Unknown") {
-          const storedCreator = localStorage.getItem(`roomCreator_${roomId}`);
-          if (storedCreator) {
-            const creatorData: string = JSON.parse(storedCreator);
-            console.log("storedCreator", storedCreator);
-            console.log("userName", userName);
-            console.log("storedCreator === userName", creatorData === userName);
-            // setRoomCreator(creatorData);
-            setIsRoomCreator(creatorData === userName);
-          }
-        }
-      }
-    );
-  };
+  //       // Set opponent player if there are enough players
+  //       if (data.players.length > 1) {
+  //         setOpponentPlayer(data.players[0]);
+  //       }
+
+  //       // Update isRoomCreator based on the creator username
+  //       const currentUserIsCreator = data.creator?.username === userName;
+  //       setIsRoomCreator(currentUserIsCreator);
+
+  //       if (data.creator.username && data.creator.username === "Unknown") {
+  //         const storedCreator = localStorage.getItem(`roomCreator_${roomId}`);
+  //         if (storedCreator) {
+  //           const creatorData: string = JSON.parse(storedCreator);
+  //           console.log("storedCreator", storedCreator);
+  //           console.log("userName", userName);
+  //           console.log("storedCreator === userName", creatorData === userName);
+  //           // setRoomCreator(creatorData);
+  //           setIsRoomCreator(creatorData === userName);
+  //         }
+  //       }
+  //     }
+  //   );
+  // };
 
   const getUsername = () => {
     // Ensure the code only runs in the browser
@@ -86,16 +116,24 @@ const StartGamePoolPrivate = (props: StartGamePoolPrivateProps) => {
     }
   };
 
-  const handleJoinRoom = () => {
-    console.log("roomId: ", roomId);
-
-    getUsername();
-    console.log("UserName: ", userName);
-    if (roomId && userName) {
-      SocketManager.joinRoom(roomId, isRoomPrivate, userName);
-      console.log("Joined to the Room : ", roomId);
+  useEffect(() => {
+    // Ensure roomdataFromDB is available and the user hasn't already joined the room
+    if (roomdataFromDB) {
+      handleJoinRoom(); // Call handleJoinRoom once roomdataFromDB is available
+      hasJoinedRoom.current = true; // Set to true to prevent future calls
     }
-  };
+  }, [roomdataFromDB]); // Add roomdataFromDB as a dependency
+
+  // const handleJoinRoom = () => {
+  //   console.log("roomId: ", roomId);
+
+  //   getUsername();
+  //   console.log("UserName: ", userName);
+  //   if (roomId && userName) {
+  //     SocketManager.joinRoom(roomId, isRoomPrivate, userName);
+  //     console.log("Joined to the Room : ", roomId);
+  //   }
+  // };
 
   const handleEnterUserName = () => {
     if (username?.trim()) {
@@ -104,36 +142,66 @@ const StartGamePoolPrivate = (props: StartGamePoolPrivateProps) => {
     }
   };
 
-  useEffect(() => {
-    if (webSocketURL)
-      // Connect to socket on mount
-      SocketManager.connect(webSocketURL);
+  const handleJoinRoom = () => {
     getUsername();
-
-    handleJoinRoom();
-    getRoomData();
-
-    // Listen for player-joined events
-    SocketManager.onPlayerJoined((newRoomData: SocketData[]) => {
-      setRoomData(newRoomData);
-      if (newRoomData.length > 1) {
-        setOpponentPlayer(newRoomData[1]); // Update opponent player
+    if (roomId && userName) {
+      const roomName = roomId;
+      // SocketManager.joinRoom(roomId, isRoomPrivate, userName);
+      // Only update the database if the player hasn't joined before
+      console.log("roomdataFromDB", roomdataFromDB);
+      if (roomdataFromDB) {
+        const alreadyJoined = roomdataFromDB.playerUserNames.some(
+          (player: string) => player === userName
+        );
+        if (!alreadyJoined) {
+          // Updating the database
+          joinRoomDB({
+            userName,
+            roomName,
+          });
+        }
+        hasJoinedRoom.current = true;
       }
-    });
+    }
+  };
 
-    // Handle game start event
-    SocketManager.onGameStart(() => {
-      router.push(`/multiplayer/gameplay/public/${roomId}`);
-    });
+  // useEffect(() => {
+  //   if (webSocketURL)
+  //     // Connect to socket on mount
+  //     SocketManager.connect(webSocketURL);
+  //   getUsername();
 
-    // Disconnect socket when component unmounts
-    return () => {
-      SocketManager.disconnect();
-    };
-  }, [webSocketURL, roomId, userName]);
+  //   handleJoinRoom();
+  //   getRoomData();
+
+  //   // Listen for player-joined events
+  //   SocketManager.onPlayerJoined((newRoomData: SocketData[]) => {
+  //     setRoomData(newRoomData);
+  //     if (newRoomData.length > 1) {
+  //       setOpponentPlayer(newRoomData[1]); // Update opponent player
+  //     }
+  //   });
+
+  //   // Handle game start event
+  //   SocketManager.onGameStart(() => {
+  //     router.push(`/multiplayer/gameplay/public/${roomId}`);
+  //   });
+
+  //   // Disconnect socket when component unmounts
+  //   return () => {
+  //     SocketManager.disconnect();
+  //   };
+  // }, [webSocketURL, roomId, userName]);
 
   const handleStartGame = () => {
-    SocketManager.emitGameStart(roomId); // Emit the event for start game
+    console.log("start");
+    const roomName = roomId;
+    updateRoomStatustoJoined({
+      roomName,
+    });
+    console.log("players joined", PlayersJoined);
+
+    // SocketManager.emitGameStart(roomId); // Emit the event for game start
   };
 
   return (
@@ -141,15 +209,25 @@ const StartGamePoolPrivate = (props: StartGamePoolPrivateProps) => {
       {userName ? (
         <div>
           <div className="flex  justify-center gap-20 p-20 mt-10">
-            <div className="bg-muted/100 rounded-full p-20 ">{userName}</div>
-            <div className="bg-muted/100  rounded-full p-20 ">
-              {opponentPlayer?.username}
+            <div className="flex  justify-center gap-20 p-20 mt-10">
+              {isOpponentJoinedDB && userName ? (
+                <>
+                  <MyName />
+                  <OpponentsName userName={userName} roomName={roomId} />
+                </>
+              ) : (
+                <>
+                  <MyName />
+                </>
+              )}
             </div>
           </div>
           <div className=" h-full flex justify-center items-center">
-            <div className={`p-20 mt-20 ${isRoomCreator ? "flex" : " hidden"}`}>
+            <div
+              className={`p-20 mt-20 ${isRoomCreatorDB ? "flex" : " hidden"}`}
+            >
               <Button
-                disabled={!opponentPlayer}
+                disabled={!isOpponentJoinedDB}
                 className="h-20 w-80 rounded-2xl"
                 onClick={handleStartGame}
               >

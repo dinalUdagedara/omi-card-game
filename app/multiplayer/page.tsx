4cdf9eb6c7b92f0dee2c514dbedf77b0d2e87e6f
Thiyunu <dinal.bandara@gmail.com>
2024-10-24 19:12:45 +0530

    style: Added a image to add a smooth effect to the image in the /multiplayer

--- app/multiplayer/page.tsx
+++ app/multiplayer/page.tsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import Image from "next/image";
 import wizardImage from "@/public/assets/images/wizard-character.png";
+import wizardCover from "@/public/assets/images/backgrounds/wizard-cover.png";
 
 const Multiplayer = () => {
   const userName = MultiplayerStateStore((state) => state.userName);
@@ -23,14 +24,26 @@ const Multiplayer = () => {
         <PoolUI />
       ) : (
         <div className="h-full min-h-screen flex flex-col lg:flex-row  gap-20 lg:gap-40 justify-center items-center">
-          <div className="hidden lg:flex z-20">
+          <div className="relative hidden lg:flex z-20">
             <Image
               src={wizardImage}
               width={400}
               height={400}
               quality={100}
-              alt="Picture of the author"
+              alt="Wizard Image"
+              className="relative z-10"
             />
+            {/* <div className="absolute bottom-0 left-0 right-0 h-24 bg-gradient-to-b from-transparent to-black/20"></div> */}
+
+            {/* Smoke SVG overlay */}
+            <div className="absolute bottom-0 left- right- h-40  z-20 -mb-10">
+              <Image
+                src={wizardCover}
+                alt="Smoke Effect"
+                height={500}
+                width={500}
+              />
+            </div>
           </div>
 
           <UserNameInput />

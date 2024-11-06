b6b124d858675dc857572d86c2d3d60d7212820e
Thiyunu <dinal.bandara@gmail.com>
2024-11-06 12:03:07 +0530

    feat: confirming if player wants to continue a violation

--- components-multiplayer/game-play/decks/user-deck-multiplayer.tsx
+++ components-multiplayer/game-play/decks/user-deck-multiplayer.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { motion } from "framer-motion";
 import { useStore } from "@/store/state";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
@@ -10,6 +10,18 @@ import CardComponentMultiplayer from "@/components-multiplayer/cards/card-multip
 import { checkIfViolationOccured } from "@/utils/multiplayer/game-logic-multiplayer";
 import { MultiplayerStateStore } from "@/store/multiplayer-state";
 import { useHoverSound } from "@/utils/play-sounds";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import modeCardBackground from "@/public/assets/images/mode-card-background.png";
+import notificaitonBackGround from "@/public/assets/images/cover-notification.png";
+import Image from "next/image";
 
 interface UserDeckProps {
   userID: Id<"players">;
@@ -25,6 +37,12 @@ export function UserDeckMobileMultiplayer({ userID, roomName }: UserDeckProps) {
   const setMyCardDeck = MultiplayerStateStore((state) => state.setMyCardSet);
   const muted = useStore((state) => state.muted);
   const { playHoverSound } = useHoverSound();
+
+  const [selectedCard, setSelectedCard] = useState<cardMultiplayer | null>(
+    null
+  );
+  const [isViolation, setIsViolation] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
 
   const myCardSet = useQuery(api.gameStates.getMyCardSet, {
     playerId: userID,
@@ -67,36 +85,124 @@ export function UserDeckMobileMultiplayer({ userID, roomName }: UserDeckProps) {
   }, [myCardSet]);
 
   async function handleCardSelect(card: cardMultiplayer) {
+    const userId = userID;
     // update this player status to "playing"
     updatePlayerStatus({
       status: "playing",
       userId: userID,
     });
     if (myCardSet && turnsuit) {
-      const validationOccured = checkIfViolationOccured(
+      const violationOccured = checkIfViolationOccured(
         card,
         myCardSet,
         turnsuit
       );
-      if (validationOccured && userName && myTeam) {
-        updateViolation({
-          roomName: roomName,
-          teamNumber: myTeam,
-          userName: userName,
-          violation: "Played a Diffferent Card from the Turn Suit",
+      if (violationOccured) {
+        // A violation occurred, ask for confirmation
+        setSelectedCard(card);
+        setIsViolation(true);
+        setOpenDialog(true); // Open the confirmation dialog
+      } else {
+        await selectCard({
+          card,
+          roomName,
+          userId,
         });
       }
+    } else {
+      //Selecting card if there are no turn suits yet
+      await selectCard({
+        card,
+        roomName,
+        userId,
+      });
     }
-    const userId = userID;
-    await selectCard({
-      card,
-      roomName,
-      userId,
-    });
+  }
+
+  // Function to handle when the user confirms they want to proceed
+  async function handleConfirmSelection() {
+    if (selectedCard && userName && myTeam) {
+      // Update the violation before proceeding
+      updateViolation({
+        roomName: roomName,
+        teamNumber: myTeam,
+        userName: userName,
+        violation: "Played a Different Card from the Turn Suit",
+      });
+
+      await selectCard({
+        card: selectedCard,
+        roomName,
+        userId: userID,
+      });
+
+      setOpenDialog(false); // Close the dialog
+      setSelectedCard(null); // Reset the selected card
+      setIsViolation(false); // Reset violation flag
+    }
+  }
+
+  // Function to handle when the user cancels the selection
+  function handleCancelSelection() {
+    setOpenDialog(false); // Close the dialog
+    setSelectedCard(null); // Reset the selected card
+    setIsViolation(false); // Reset violation flag
   }
 
   return (
     <div className="h-2 w-96 flex justify-center mr-5 ">
+      <div className="">
+        <Dialog open={isViolation}>
+          {/* <Dialog> */}
+          {/* <DialogTrigger className=" h-20 w-20">Open</DialogTrigger> */}
+          <DialogContent className="real w-[360px] lg:w-full max-w-[450px] inv-rad inv-rad-6">
+            <Image
+              className="inv-rad inv-rad-6"
+              alt="Mountains"
+              src={modeCardBackground}
+              fill
+              sizes="(min-width: 808px) 50vw, 100vw"
+              style={{
+                objectFit: "fill",
+              }}
+            />
+            <Image
+              className="p-3 inv-rad inv-rad-8"
+              alt="Mountains"
+              src={notificaitonBackGround}
+              fill
+              sizes="(min-width: 808px) 50vw, 100vw"
+              style={{
+                objectFit: "fill",
+              }}
+            />
+            <DialogHeader className="z-20 text-black">
+              <DialogTitle className="flex justify-center font-bold text-2xl">
+                Are you sure?
+              </DialogTitle>
+              <DialogDescription className="flex justify-center text-black font-semibold">
+                Do you Want to Select the Card {selectedCard?.value} of{" "}
+                {selectedCard?.suit}
+              </DialogDescription>
+            </DialogHeader>
+            <div className="flex w-full justify-end gap-2 z-20 px-5">
+              <Button
+                className="bg-amber-950 h-8 text-white  hover:bg-amber-800  text-md"
+                onClick={handleConfirmSelection}
+              >
+                Yes
+              </Button>
+              <Button
+                className="bg-amber-950 h-8 text-white  hover:bg-amber-800  text-md"
+                onClick={handleCancelSelection}
+              >
+                No
+              </Button>
+            </div>
+          </DialogContent>
+        </Dialog>
+      </div>
+
       <div className="flex justify-center">
         {myCardSet?.map((card, index) => {
           const angle = (index - (myCardSet.length - 1) / 2) * 10;
@@ -157,6 +263,14 @@ export function UserDeckMobileMultiplayer({ userID, roomName }: UserDeckProps) {
           );
         })}
       </div>
+
+      {/* <div className="bg-white z-20">
+        Violation Occured Do you Want to continue
+        <div>
+          <Button onClick={handleConfirmSelection}>Yes</Button>
+          <Button onClick={handleConfirmSelection}>No</Button>
+        </div>
+      </div> */}
     </div>
   );
 }

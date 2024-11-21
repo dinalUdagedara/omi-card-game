9d3d0c01ed8d0e1f4a543484b1f2f15ec173aa7f
Thiyunu <dinal.bandara@gmail.com>
2024-11-21 17:31:18 +0530

    style: changed colors of progression ring

--- components-multiplayer/game-play/user-avatars/avatar-with-timeout.tsx
+++ components-multiplayer/game-play/user-avatars/avatar-with-timeout.tsx
@@ -8,7 +8,8 @@ import { api } from "@/convex/_generated/api";
 import { usePathname } from "next/navigation";
 import notificaitonBackGround from "@/public/assets/images/cover-notification.png";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import 'react-circular-progressbar/dist/styles.css';
+import "react-circular-progressbar/dist/styles.css";
+import { useStore } from "@/store/state";
 
 const UserAvatarContainerWithTimeOut = () => {
   const pathname = usePathname();
@@ -16,17 +17,23 @@ const UserAvatarContainerWithTimeOut = () => {
   const playerTurnUserName = useQuery(api.gameLogic.getPlayerTurnName, {
     roomName: roomId || "",
   });
-  const [seconds, setSeconds] = useState(15); // Initial value set to 10 seconds
+  const noOfPlayingcards = useQuery(api.gameLogic.noOfPlayingCards, {
+    roomName: roomId || "",
+  });
+  const trumpSuit = useStore((state) => state.trumpSuit);
+  const [seconds, setSeconds] = useState(15); // Initial value set to 15 seconds
   // Reset timer whenever playerTurnUserName changes
   useEffect(() => {
-    setSeconds(15); // Reset to 15 seconds
+    const num = noOfPlayingcards ?? 0;
+    if (num < 4 && trumpSuit) {
+      setSeconds(15); // Reset to 15 seconds
 
-    const interval = setInterval(() => {
-      setSeconds((prev) => prev - 1);
-    }, 1000);
-
-    return () => clearInterval(interval); // Cleanup the interval on component unmount
-  }, [playerTurnUserName]); // Trigger when playerTurnUserName changes
+      const interval = setInterval(() => {
+        setSeconds((prev) => prev - 1);
+      }, 1000);
+      return () => clearInterval(interval); // Cleanup the interval on component unmount
+    }
+  }, [playerTurnUserName, noOfPlayingcards, trumpSuit]);
 
   useEffect(() => {
     if (seconds === 0) return; // Stop the timer when it reaches 0
@@ -35,18 +42,23 @@ const UserAvatarContainerWithTimeOut = () => {
   return (
     <div>
       <CircularProgressbarWithChildren
+        styles={{
+          path: {
+            stroke: "#3e2723", // Dark brown color for the progress path
+            strokeLinecap: "round", // Rounded corners for a smooth finish
+          },
+          trail: {
+            stroke: "#d7ccc8", // Light beige/cream color for the background trail
+          },
+        }}
         className="relative w-16 h-16 lg:w-32 lg:h-32 z-20"
-        value={seconds * 6}
+        value={seconds * 6.666}
       >
         <motion.div
           className=" rounded-full"
           initial={{ boxShadow: "none" }}
           animate={{
-            boxShadow:
-              //   playerTurnUserName === opponent_2
-              //     ? "0 0 16px rgba(0, 255, 0, 0.8)" // Green glowing effect
-              //     : "none", // No shadow when it's not players's turn
-              "0 0 16px rgba(0, 255, 0, 0.8)", // Green glowing effect
+            boxShadow: "0 0 20px rgba(255, 255, 255, 0.8)", // White glowing effect
           }}
           transition={{
             duration: 0.8,
